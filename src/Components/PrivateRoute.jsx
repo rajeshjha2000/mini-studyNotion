fef0bff4126d72b9644f31710662335aeba0ce0e
rajeshjha2000 <rajeshkumarjha153@gmail.com>
2024-02-27 22:06:57 +0530

    first commit

---
+++ src/Components/PrivateRoute.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Navigate } from 'react-router-dom';
+
+const PrivateRoute = ({ children, isLoggedIn }) => {
+    if (isLoggedIn) {
+        return children;
+    }
+    else {
+        return <Navigate to="/login" />;
+    }
+    
+}
+
+export default PrivateRoute
